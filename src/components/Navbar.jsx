08fde4de4a4d578eadc4214209d7091ae4785624
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-11-05 13:41:08 +0530

    REDONE THE DROPDOWN WITH SOME CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     return (
         <>
             <div className="relative">
-                <nav className="absolute inset-x-0 top-0 z-50 m-4 font-semibold text-gray-400 font sm:my-20 sm:mx-52 ">
-                    <ul className="flex flex-wrap justify-between ">
+                <nav className="absolute inset-x-0 top-0 z-50 m-4 font-semibold tracking-wider text-gray-400 sm:my-20 sm:mx-52">
+                    <ul className="flex flex-wrap justify-between py-5">
 
                         <li className="relative group">
-                            <p className="duration-300 cursor-pointer hover:text-black">SERVICES</p>
-                            <div className="absolute w-52  bg-[#EDEDED] rounded-lg cursor-pointer  hidden group-hover:block ">
-                                <ul className="top-0 mt-2 ">
+                            <p className="py-2 mb-2 duration-100 ease-in border-black cursor-pointer hover:border-b-2 hover:text-black">SERVICES</p>
+                            <div className="absolute w-52 left-[-90%]  bg-[#EDEDED] rounded-lg cursor-pointer  hidden group-hover:block group-hover:bg-black group-hover:duration-300  ">
+                                <ul className="top-0 ">
                                     <li className="block py-3 text-center border-b-[1.5px] border-transparent hover:border-gray-400 hover:bg-white duration-200">WEDDING</li>
                                     <li className="block py-3 text-center border-b-[1.5px] border-transparent hover:border-gray-400 hover:bg-white duration-200" >FAMILY</li>
                                     <li className="block py-3 text-center border-b-[1.5px] border-transparent hover:border-gray-400 hover:bg-white duration-200" >NEW BORN</li>
@@ -21,12 +21,15 @@ const Navbar = () => {
                             </div>
 
                         </li>
-                        <li className="duration-100 cursor-pointer hover:text-black ">BLOG</li>
-                        <li className="duration-100 cursor-pointer hover:text-black">LOGO</li>
-                        <li className="relative group">
-                            <p className="duration-100 cursor-pointer hover:text-black">INFO</p>
-                            <div className="absolute w-52  bg-[#EDEDED] rounded-lg cursor-pointer  hidden group-hover:block  ">
-                                <ul className="top-0 ">
+
+                        <li className="py-2 mb-2 duration-100 ease-in border-black cursor-pointer hover:border-b-2 hover:text-black">BLOG</li>
+
+                        <li className="py-2 mb-2 duration-100 ease-in border-black cursor-pointer hover:border-b-2 hover:text-black ">LOGO</li>
+
+                        <li className="relative duration-100 group">
+                            <p className="py-2 mb-2 duration-100 ease-in border-black cursor-pointer hover:border-b-2 hover:text-black">INFO</p>
+                            <div className="absolute  w-52  py-5  left-[-200%] bg-[#EDEDED] rounded-lg cursor-pointer  hidden group-hover:block  group-hover:bg-black group-hover:duration-300 ">
+                                <ul className="text-base ">
                                     <li className="block py-3 text-center border-b-[1.5px] border-transparent hover:border-gray-400 hover:bg-white duration-200">THE PHOTOGRAPHERS
                                     </li>
                                     <li className="block py-3 text-center border-b-[1.5px] border-transparent hover:border-gray-400 hover:bg-white duration-200" >GOOD WORDS
@@ -35,9 +38,9 @@ const Navbar = () => {
 
                                 </ul>
                             </div>
-
                         </li>
-                        <li className="duration-100 cursor-pointer hover:border-b-2 hover:border-black hover:text-black">CONTACT</li>
+
+                        <li className="py-2 mb-2 duration-100 ease-in border-black cursor-pointer hover:border-b-2 hover:text-black ">CONTACT</li>
 
                     </ul>
                 </nav>
